cb848e4bfa086dd942f92a731f18513b5fe93821
dionysus <dionysus.cho@gmail.com>
2020-09-04 16:21:13 -0700

    camera focuses on player (add'n)

---
+++ Assets/RandomMapGen/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private MapMovementController movementController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementController = GetComponent<MapMovementController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow)){
+            Debug.Log("Up key is pressed");
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow)){
+            Debug.Log("Right key is pressed");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow)){
+            Debug.Log("Down key is pressed");
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)){
+            Debug.Log("Left key is pressed");
+        }
+    }
+}
